bd547164bf13d8410d5ab436d68a0bb5d9e18c5e
Sabitha234 <sabimaniraj@gmail.com>
2023-06-30 13:27:50 +0530

    first commit

---
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/payload/CategoryDto.java
@@ -0,0 +1,45 @@
+package com.springboot.blog.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDto {
+	
+    public CategoryDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public CategoryDto(Long id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	private Long id;
+    private String name;
+    private String description;
+}
---
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/payload/RegisterDto.java
@@ -0,0 +1,53 @@
+package com.springboot.blog.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterDto {
+	
+    public RegisterDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public RegisterDto(String name, String username, String email, String password) {
+		super();
+		this.name = name;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	private String name;
+    private String username;
+    private String email;
+    private String password;
+}
